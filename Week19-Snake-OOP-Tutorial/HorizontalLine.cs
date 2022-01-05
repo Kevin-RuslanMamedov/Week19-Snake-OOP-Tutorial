95823923d190a5858c766689f8f3c31d070046be
Opilane TTHK <opilane@tthk.ee>
2022-01-05 16:06:39 +0200

    week19-snake

---
+++ Week19-Snake-OOP-Tutorial/HorizontalLine.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Week19_Snake_OOP_Tutorial
+{
+    class HorizontalLine
+    {
+        List<Point> pointList;
+
+        public HorizontalLine(int xLeft, int xRight, int y, char symb)
+        {
+            pointList = new List<Point>();
+
+            for (int i = xLeft; i <= xRight; i++)
+            {
+                Point p = new Point(i, y, symb);
+                pointList.Add(p);
+            } 
+            // ########
+        }
+        public void Draw()
+        {
+            foreach(Point point in pointList)
+            {
+                Console.SetCursorPosition(point.x, point.y);
+                Console.Write(point.symb);
+            }
+        }
+
+    }
+}
---
+++ Week19-Snake-OOP-Tutorial/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace Week19_Snake_OOP_Tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point(10, 10, '*');
+            Point p2 = new Point(11, 10, '*');
+
+            HorizontalLine hLine = new HorizontalLine(10, 14, 5, '*');
+            hLine.Draw();
+
+            Console.ReadLine();
+        }
+
+    }
+}
